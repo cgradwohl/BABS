287845a7af6de5448b9f2754ffd410d70830b7b6
Chris Gradwohl <christophergradwohl@gmail.com>
2017-03-28 17:44:36 -0700

    added new plots based on subscriber and weekend data

--- dandp0-bikeshareanalysis/analysis.py
+++ dandp0-bikeshareanalysis/analysis.py
@@ -1,4 +1,5 @@
 #	#	#	#	#	#	#	#	#	#	#	#	#	#	#	#	#	#	#	#
+# Chris Gradwohl March 2017
 # analysis.py
 # exploration file for the open Bay Area Bike Share Data set
 # in this exploration we are going to look at a subset of trip data to
@@ -78,8 +79,6 @@ print '\n\n\nStation Data file header: \n'
 # Display the first few rows of the station data file.
 station_info = pd.read_csv('201402_station_data.csv')
 display(station_info.head())
-
-
 
 # Since it is possible that more stations are added or dropped over time,
 # this function will allow us to combine the station information across
@@ -199,3 +198,73 @@ usage_plot(trip_data, 'duration', ['duration < 60'])
 
 # still need to clean up the presentation, here we corect bin boundaries and edges
 usage_plot(trip_data, 'duration', ['duration < 60'], boundary = 0, bin_width = 5)
+
+
+
+
+
+
+
+
+# process' data into a single data file
+station_data = ['201402_station_data.csv',
+                '201408_station_data.csv',
+                '201508_station_data.csv' ]
+trip_in = ['201402_trip_data.csv',
+           '201408_trip_data.csv',
+           '201508_trip_data.csv' ]
+trip_out = 'babs_y1_y2_summary.csv' # <-- new data file :)
+
+# This function will take in the station data and trip data and
+# write out a new data file to the name listed above in trip_out.
+summarise_data(trip_in, station_data, trip_out)
+
+# read and print the data header
+trip_data = pd.read_csv('babs_y1_y2_summary.csv')
+print 'New Condensed Data File'
+display(trip_data.head
+
+
+
+# Use the usage_stats() and usage_plot() methods from
+# babs_visualizations.py to exlpore the data set and report
+# new findings.
+
+# usage_plot(first, second, third) , where:
+ 		# first(required)  - loaded data frame for data analysis
+		# second(required) - variable, trip counts will be divided
+		# third(optional)  - data filters limiting the data points
+		# 					that will be counted. Filters should be given as a list
+		# 					of conditions, each element should be a string in the
+		# 					following format: '<field> <op> <value>' using one of
+		# 					the following operations: >, <, >=, <=, ==, !=. Data
+		# 					points must satisfy all conditions to be counted or
+		# 					visualized. For example, ["duration < 15",
+		# 					"start_city == 'San Francisco'"] retains only trips that
+		# 					 originated in San Francisco and are less than 15 minutes
+		# 					long.
+
+# additional parameters
+# "n_bins" specifies the number of bars in the resultant plot (default is 10).
+# "bin_width" specifies the width of each bar (default divides the range of the data by number of bins).
+# "n_bins" and "bin_width" cannot be used simultaneously.
+# "boundary" specifies where one of the bar edges will be placed; other bar edges will be placed around
+# that value (this may result in an additional bar being plotted). This argument may be used alongside
+# the "n_bins" and "bin_width" arguments.
+
+
+
+# Exploretory analysis with usage_plot() and usage_stats()
+usage_plot(trip_data)
+
+usage_stats(trip_data)
+
+
+
+# Final Plot 1
+usage_plot(trip_data)
+
+
+
+# Final Plot 2
+usage_plot(trip_data)
